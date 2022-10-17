87e8b89b4a878b88f5e393d8c41172cf442f0037
Chekan-Liza <70697985+Chekan-Liza@users.noreply.github.com>
2022-10-17 23:21:28 +0300

    fix: email solution

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -1,4 +1,21 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Given an email address, return it's domain.
+ *
+ * @param {String} email
+ * @return {String}
+ *
+ * @example
+ * For the input 'prettyandsimple@example.com', the output should be 'example.com'
+ *
+ */
 function getEmailDomain(m) {
   let a = m.split('@');
   return a[a.length-1];
 }
+
+
+module.exports = {
+  getEmailDomain
+};
