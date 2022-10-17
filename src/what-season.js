a1d188fe85181d1920b2d86778de891826870976
Chekan-Liza <70697985+Chekan-Liza@users.noreply.github.com>
2022-10-17 20:52:48 +0300

    fix: data solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,29 @@
-const { NotImplementedError } = require('../extensions/index.js');
+function getSeason(date) {
+  const winter = [1, 2, 12,];
+  const spring = [3, 4, 5,];
+  const summer = [6, 7, 8,];
+  const autumn = [9, 10, 11,];
 
-/**
- * Extract season from given date and expose the enemy scout!
- * 
- * @param {Date | FakeDate} date real or fake date
- * @returns {String} time of the year
- * 
- * @example
- * 
- * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
- */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if(date){
+    let str = new Date(866, 2, 10, 12, 53, 10, 825).toISOString();
+    let new_str = str.substring(str.lastIndexOf("-"), 2);
+    let month = new_str.substr(-2);
+    
+    if(month[0] == '0'){
+      month = month.substr(-1);
+    }
+    
+    for(let w in winter){
+      if(+month == winter[w]) return 'winter';
+    }
+    for(let s in spring){
+      if(+month == spring[s]) return 'spring';
+    }
+    for(let sum in summer){
+      if(+month == summer[sum]) return 'summer';
+    }
+    for(let a in autumn){
+      if(+autumn == autumn[a]) return 'autumn';
+    }
+  }else return 'Unable to determine the time of year!';
 }
-
-module.exports = {
-  getSeason
-};
